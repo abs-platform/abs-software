d5be5ff207dccba3128770c062761daf184befe3
soniagudayol <sonia.gudayol@gmail.com>
2015-04-01 18:10:38 +0200

    Corrected test, modified json file and added the function to identify the type of command

--- json_translator/test.c
+++ json_translator/test.c
@@ -7,41 +7,74 @@
 #include <stdlib.h>
 #include "cJSON.h"
 
+#define TYPE_MESSAGE 1
+#define TYPE_STATE 2
+#define TYPE_PAYLOAD 3
+#define TYPE_LOCATION 4
+
+int identify_type (cJSON *json){
+    /*Access the fifth child, always the type*/
+    int i;
+    cJSON *subitem = json->child;
+    for(i = 0; i < TYPE_LOCATION; i++) subitem = subitem->next;
+    i = printf("%s\n", subitem->valuestring);
+    if(strcmp(subitem->valuestring, "message") == 0) return TYPE_MESSAGE;
+    else if(strcmp(subitem->valuestring, "state")) return TYPE_STATE;
+    else return TYPE_PAYLOAD;
+}
+
+void translate_message (cJSON *json){
+    //...
+}
+
+void translate_state (cJSON *json){
+    //...
+}
+
+void translate_payload (cJSON *json){
+    //...
+}
+
 void main (){
-    int a; FILE *f; long len; char *data; cJSON *json;
+    int a, type_id; 
+    FILE *f; 
+    long len; 
+    char *data; 
+    cJSON *json;
 
     /*For running and building purposes*/
     a = printf("Start main\n");
     
     /*Load the .json file to char*/
-    f = fopen("mcs_conf_file.json", "rb"); fseek(f, 0, SEEK_END); 
+    f = fopen("mcs_conf_file.json", "rb"); /*L.17 "b" in opening mode makes no effect!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+    fseek(f, 0, SEEK_END); 
     len = ftell(f);
     fseek(f, 0, SEEK_SET);
-    data = (char*)malloc(len+1); fread(data, 1, len, f); fclose(f);
+    data = (char*)malloc(len + 1); /*L.20 It is not necessary to cast the result of a malloc*/
+    fread(data, 1, len, f); 
+    fclose(f);
     
     /*Parse json and operate*/
     json = cJSON_Parse(data);
     if(!json) {
         printf("Error before: [%s]\n", cJSON_GetErrorPtr());
-    }
-    else {
-        /*Prints the first element and then looks for child, or sibling*/
-        json = json->child;
-        a = printf("First element: %s", json->string);
+    } else {
+        a = printf("Start function\n");
+        type_id = identify_type (json);
+        a = printf("End function: %d\n", type_id);
         
-        while(json->child){
-            json = json->child;
-            a = printf("\n    Child: %s", json->valuestring);
+        switch (type_id) {
+            case TYPE_MESSAGE:
+                translate_message(json);
+            case TYPE_STATE:
+                translate_state(json);
+            case TYPE_PAYLOAD:
+                translate_payload(json);
         }
         
-        while(json->next){
-            json = json->next;
-            a = printf("\n    Sibling: %s", json->valuestring);
-        }
-        cJSON_Delete(json);
     }
     
     free(data);
     
-    a = printf("\nEnd main");
+    a = printf("\nEnd main===================================================\n");
 }
