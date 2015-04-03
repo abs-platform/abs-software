10c46eba3ef709f2d4b660a729f019fa72ff2759
soniagudayol <sonia.gudayol@gmail.com>
2015-04-03 12:27:20 +0200

    Complete test of ttranslating a single-element json file

---
+++ json_translator/printed.c
@@ -0,0 +1,16 @@
+/*HERE BEGINS THE PRINTED FILE, commented for building p
+
+const static struct MCSCommandMessage mcs_command_message_list[] =
+{
+    {
+    .cmd = {
+        .name = "procman_start",
+        .nargs = 1,
+        .raw_data = false,
+        .response_size = 0,
+    },
+    .destination = "procman",
+    .origin_groups = {syscore},
+    .destination_groups = NULL,
+    }
+*/
--- json_translator/test.c
+++ json_translator/test.c
@@ -1,7 +1,6 @@
-/*This is a test file to start working on and identify the parsed c structures.
- Taking the functions from cJSON's library tests as an example, this will read
- a simple json structured file, identify its parameters and print them on screen
- with their correct labels*/
+/*This is a test file designed to take a single-element json, identify it, and
+ print its equivalent in C (commented for building purposes). Next will come
+ working with various elements in the json file*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,6 +13,7 @@
 
 int identify_type (cJSON *json){
     int i;
+    int res;
     cJSON *subitem = json->child;
     for(i = 0; i < TYPE_LOCATION; i++) subitem = subitem->next;
     i = printf("%s\n", subitem->valuestring);
@@ -23,19 +23,61 @@ int identify_type (cJSON *json){
 }
 
 void translate_message (cJSON *json){
-    //...
-    FILE *ofp;
-    ofp = fopen("printed.c", "w");
-    fprintf(ofp, "//Hello World");
-    fclose (ofp);
+    FILE *out;
+    cJSON *subitem;
+
+    out = fopen("printed.c", "w");
+    fprintf(out, "/*HERE BEGINS THE PRINTED FILE, commented for building p\n\n");
+    
+    fprintf(out, "const static struct MCSCommandMessage "
+            "mcs_command_message_list[] =\n{\n    {\n");
+    json = json->child;
+    fprintf(out, "    .cmd = {\n        .name = \"%s\",\n", json->valuestring);
+    json = json->next->next;
+    fprintf(out, "        .nargs = %d,\n", json->valueint);
+    json = json->next;
+    if(json->valueint) fprintf(out, "        .raw_data = true,\n");
+    else fprintf(out, "        .raw_data = false,\n");
+    json = json->next->next->child;
+    subitem = json->next->next->next;
+    fprintf(out, "        .response_size = %d,\n", subitem->valueint);
+    fprintf(out, "    },\n    .destination = \"%s\",\n", json->valuestring);
+    json = json->next;
+    if(json->child){
+        subitem = json->child;
+        fprintf(out, "    .origin_groups = {%s", subitem->valuestring);
+        while(subitem->next){
+            subitem = subitem->next;
+            fprintf(out, ", %s", subitem->valuestring);
+        }
+        fprintf(out, "},\n");
+    } else {
+        fprintf(out, "    .origin_groups = NULL,\n");
+    }
+    json = json->next;
+    if (json->child) {
+        subitem = json->child;
+        fprintf(out, "    .destination_groups = {%s", subitem->valuestring);
+        while (subitem->next) {
+            subitem = subitem->next;
+            fprintf(out, ", %s", subitem->valuestring);
+        }
+        fprintf(out, "},\n");
+    } else {
+        fprintf(out, "    .destination_groups = NULL,\n");
+    }
+    fprintf(out, "    }");
+    
+    fprintf(out, "\n*/");
+    fclose (out);
 }
 
 void translate_state (cJSON *json){
-    //...
+    /*...*/
 }
 
 void translate_payload (cJSON *json){
-    //...
+    /*...*/
 }
 
 void main (){
@@ -49,11 +91,11 @@ void main (){
     a = printf("Start main\n");
     
     /*Load the .json file to char*/
-    f = fopen("mcs_conf_file.json", "rb"); /*L.17 "b" in opening mode makes no effect!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+    f = fopen("mcs_conf_file.json", "rb"); /*CORRECT: L.17 "b" in opening mode makes no effect*/
     fseek(f, 0, SEEK_END); 
     len = ftell(f);
     fseek(f, 0, SEEK_SET);
-    data = (char*)malloc(len + 1); /*L.20 It is not necessary to cast the result of a malloc*/
+    data = (char*)malloc(len + 1); /*CORRECT: L.20 It is not necessary to cast the result of a malloc*/
     fread(data, 1, len, f); 
     fclose(f);
     
@@ -77,7 +119,8 @@ void main (){
         
     }
     
+    cJSON_Delete(json);
     free(data);
     
-    a = printf("\nEnd main===================================================\n");
+    a = printf("\nEnd main\n");
 }
